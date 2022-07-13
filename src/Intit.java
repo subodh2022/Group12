4bd658369a7eb60e331a4f0c378c264382ac4669
Subodh Adav <subodh.adav.007@gmail.com>
2022-07-13 11:19:40 +0530

    This is first commit file of our Group that is Group12

---
+++ src/Intit.java
@@ -0,0 +1,27 @@
+
+public class Intit {
+
+	private static final String TeamName = null;
+
+	public static void main(String[] args) {
+		
+		String Team_P1 = "subodh";
+		String Team_P2 = "suraj";
+		String Team_P3 = "krutika";
+		String Team_P4 = "prajakta";
+		String Team_P5 = "amruta";
+		
+		
+		String TeamName = "Groip_12";
+		Team();
+		
+		
+
+	}
+	
+	public static void Team()
+	{
+		System.out.println("welcome gusys in our group" +TeamName );
+	}
+
+}
